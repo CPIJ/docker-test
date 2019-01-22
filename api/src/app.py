f704411251f101b31a4567c9900995772b25dbc6
Casper Pijnenburg <casperpijnenburg@gmail.com>
2019-01-22 20:42:22 +0100

    added database

--- api/src/app.py
+++ api/src/app.py
@@ -1,12 +1,12 @@
 from flask import Flask
+from redis import Redis
 
 app = Flask(__name__)
-
+redis = Redis(host='api-db', port=6379)
 
 @app.route('/')
 def hello():
-    return 'Hello, World!'
-
+    return redis.get('test')
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
