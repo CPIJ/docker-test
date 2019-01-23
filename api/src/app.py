4d08e4c589996d9b9a25fb1d5b34f9cd98e16b4e
Casper Pijnenburg <casperpijnenburg@gmail.com>
2019-01-23 08:56:23 +0100

    Added vue

--- api/src/app.py
+++ api/src/app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask import request
 from redis import Redis
 
 app = Flask(__name__)
@@ -7,6 +8,10 @@ redis = Redis(host='api-db', port=6379)
 @app.route('/')
 def hello():
     return redis.get('test')
+
+@app.route('/person', methods=["POST"])
+def test():
+    print(request)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
